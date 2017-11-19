3df28fe1265eac3214e6e9ad9900f4bf0df07d23
Justin Searls <searls@gmail.com>
2017-11-19 09:14:28 -0500

    1.9.9.pre1

--- lib/carat/version.rb
+++ lib/carat/version.rb
@@ -2,5 +2,5 @@ module Carat
   # We're doing this because we might write tests that deal
   # with other versions of carat and we are unsure how to
   # handle this better.
-  VERSION = "1.9.9" unless defined?(::Carat::VERSION)
+  VERSION = "1.9.9.pre1" unless defined?(::Carat::VERSION)
 end
